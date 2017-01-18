3c3d4b0df4440cb97c816e191fc137f8182aeb9c
Mike Kim <nser789@gmail.com>
2017-01-18 22:14:55 +0900

    Init

---
+++ smus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smus
+//
+//  Created by kim-yongseong on 2017. 1. 18..
+//  Copyright © 2017년 YongSeongKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
