68a63f2386ef34ff929a1c3e569ef7b693ab6bd6
Mike Kim <nser789@gmail.com>
2017-01-18 23:44:53 +0900

    Add code
    Add time data, Admob, lib

---
+++ smus/Const.swift
@@ -0,0 +1,9 @@
+//
+//  Const.swift
+//  smus
+//
+//  Created by kim-yongseong on 2017. 1. 18..
+//  Copyright © 2017년 YongSeongKim. All rights reserved.
+//
+
+let configUrl = "https://raw.githubusercontent.com/kys9261/smus2/master/smus/jsonData/config.json"
--- smus/ViewController.swift
+++ smus/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import GoogleMobileAds
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //admob
+        bannerView.adUnitID = "ca-app-pub-0528563211821066/1643960076"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+        //get config file
+        Alamofire.request(configUrl).responseJSON {
+            responseData in
+            if((responseData.result.value) != nil) {
+                let config = JSON(responseData.result.value!)
+                self.title = config["title"].stringValue
+                
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
